562420d4172f861ec71a39457f7a41fe5f18fccf
BrandonRiconga <bricongaw@gmail.com>
2025-04-08 23:53:23 +0700

    feat: Added get and post message

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const ControllUser = require('../controllers/controllUser')
+const messageController = require('../controllers/messageController')
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,5 +9,7 @@ router.get('/', (req, res) => {
 
 router.post('/register', ControllUser.register)
 router.post('/login', ControllUser.login)
+router.get('/group/:groupId', messageController.getMessage)
+router.post('/group/:groupId', messageController.postMessage)
 
 module.exports = router
