24941193f0d1308d277d16b85391514f3cd6af08
Resya-Hasan <resyahasan0@gmail.com>
2025-04-09 08:42:34 +0700

    share group via link done

--- controllers/controllGroup.js
+++ controllers/controllGroup.js
@@ -3,10 +3,10 @@ const { Group, GroupUser } = require('../models');
 
 class ControllGroup {
     static async createGroup(req, res, next) {
-        try {   
+        try {
             const { name } = req.body;
             const userId = req.user.id;
-          
+
             if (!name) {
                 throw { name: "Bad Request", message: "Group name is required" };
             }
@@ -17,16 +17,53 @@ class ControllGroup {
                 name,
                 inviteCode,
                 createdBy: userId
-              });
-          
-              await GroupUser.create({
+            });
+
+            await GroupUser.create({
                 UserId: userId,
                 GroupId: group.id,
                 role: "admin"
-              });
-          
-              res.status(201).json({ message: "Group created", group });
-        } catch(err) {
+            });
+
+            res.status(201).json({ message: "Group created", group });
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async shareGroup(req, res, next) {
+        try {
+            const { inviteCode } = req.params;
+            const userId = req.user.id;
+
+            const group = await Group.findOne({ where: { inviteCode } });
+
+            if (!group) {
+                throw { name: "NotFound", message: "Group not found" };
+            }
+
+            const isMember = await GroupUser.findOne({
+                where: { GroupId: group.id, UserId: userId }
+            });
+
+            if (isMember) {
+                throw { name: "Bad Request", message: "You are already a member of this group." };
+            }
+
+            await GroupUser.create({
+                GroupId: group.id,
+                UserId: userId,
+                role: "member"
+            });
+
+            return res.status(200).json({
+                message: "Successfully joined the group.",
+                group: {
+                    id: group.id,
+                    name: group.name
+                }
+            });
+        } catch (err) {
             next(err)
         }
     }
