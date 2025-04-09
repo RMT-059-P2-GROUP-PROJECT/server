d9d00c92af897e7c810a6d93b84922eb6f970279
Resya-Hasan <resyahasan0@gmail.com>
2025-04-09 19:09:11 +0700

    setup socket.io in server done

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 const errorHandler = require('./middleware/errorHandler')
 const cors = require('cors')
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+io.on('connection', (socket) => {
+  //...
+  console.log(socket.id, "<<<< socket id")
+})
+
 app.use('/', require('./routers/index'))
 
 app.use(errorHandler)
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
